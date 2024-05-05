c1dd3604265ca3359ab458853a3b8ca71e024307
Bawornsak <nuiwitclup@gmail.com>
2024-05-05 19:19:44 +0700

    delete user.ts

--- src/type/user.ts
+++
@@ -1,9 +0,0 @@
-interface IUserService {
-  getUsers: grpc.handleUnaryCall<GetUsersRequest, GetUsersResponse>;
-}
-
-interface GetUsersRequest {}
-
-interface GetUsersResponse {
-  users: any[]; // Define a more detailed structure based on your actual data schema
-}
